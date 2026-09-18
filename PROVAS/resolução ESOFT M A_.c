17ec4d4e897311039d57debd9d8abfe55fe7775c
ProfDacioMachado <dacio.francisco@unicesumar.edu.br>
2026-09-18 18:35:11 -0300

    Implement backpack item distribution calculation
    Added a program to calculate the number of backpacks needed for items based on capacity.

--- PROVAS/resolução ESOFT M A_.c
+++ PROVAS/resolução ESOFT M A_.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 
 int main(){
+/* EX 0 */
+    #include <stdio.h>
 
+int main()
+{
+    int capacidade, qtd_itens, n_mochilas, resto;
+    
+    printf("Insira a quantidade de itens a serem dispostos nas mochilas: \n");
+    scanf("%d",&qtd_itens);
+    printf("Insira a capacidade de itens de cada mochila: \n");
+    scanf("%d",&capacidade);
+    
+    n_mochilas = qtd_itens/capacidade;
+    resto = qtd_itens%capacidade; 
+    
+    printf("Legendario, são %d mochilas para seus itens, e sobram %d itnes", n_mochilas, resto);
+    
+ // EX 1
     return 0;
 }
